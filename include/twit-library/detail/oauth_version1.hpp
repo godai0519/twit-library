27d0a1e691cc08824f509f2c7c836f46993aff6b
Godai Azuma <godai0519@gmail.com>
2012-05-27 17:53:32 +0900

    Async Authentication is Impl.
    Signed-off-by: godai_0519 <godai0519@gmail.com>

--- include/twit-library/detail/oauth_version1.hpp
+++ include/twit-library/detail/oauth_version1.hpp
@@ -40,9 +40,9 @@ public:
     boost::assign::insert(params)
       ("oauth_signature",oauth::utility::get_signature(method,uri,key_->get_signature_key(),params));
 
-    boost::asio::streambuf buf;
+    boost::shared_ptr<boost::asio::streambuf> buf(new boost::asio::streambuf());
     {
-      std::ostream os(&buf);
+      std::ostream os(buf.get());
       os << method << " " << oauth::utility::get_path(uri) << " HTTP/1.1" << "\r\n";
       os << "Host: " << oauth::utility::get_host(uri) << "\r\n";
       os << "Content-Type: " << "application/x-www-form-urlencoded" << "\r\n";
@@ -50,15 +50,17 @@ public:
     }
 
     boost::system::error_code ec;
-    client_->operator() (oauth::utility::get_host(uri),buf,ec);
+    const boost::shared_ptr<bstcon::response> response = 
+      client_->operator() (oauth::utility::get_host(uri),buf,ec,
+        boost::bind(&oauth_version1::set_access_token,this,_1,_2));
 
-    const int status_code = client_->get_response()->status_code;
-    if(200 <= status_code && status_code < 300)
-    {
-      const Param_Type response = oauth::utility::parse_urlencoded(client_->get_response()->body);
-      key_->set_access_token (oauth::utility::url_decode(response.at("oauth_token")));
-      key_->set_access_secret(oauth::utility::url_decode(response.at("oauth_token_secret")));
-    }
+    //const int status_code = response->status_code;
+    //if(200 <= status_code && status_code < 300)
+    //{
+    //  const Param_Type parsed = oauth::utility::parse_urlencoded(response->body);
+    //  key_->set_access_token (oauth::utility::url_decode(parsed.at("oauth_token")));
+    //  key_->set_access_secret(oauth::utility::url_decode(parsed.at("oauth_token_secret")));
+    //}
 
     return;
   }
@@ -77,9 +79,9 @@ public:
     boost::assign::insert(params)
       ("oauth_signature",oauth::utility::get_signature(method,uri,key_->get_signature_key(),params));
 
-    boost::asio::streambuf buf;
+    boost::shared_ptr<boost::asio::streambuf> buf(new boost::asio::streambuf());
     {
-      std::ostream os(&buf);
+      std::ostream os(buf.get());
       os << method << " " << oauth::utility::get_path(uri) << " HTTP/1.1" << "\r\n";
       os << "Host: " << oauth::utility::get_host(uri) << "\r\n";
       os << "Content-Type: " << "application/x-www-form-urlencoded" << "\r\n";
@@ -87,14 +89,14 @@ public:
     }
 
     boost::system::error_code ec;
-    client_->operator() (oauth::utility::get_host(uri),buf,ec);
+    const boost::shared_ptr<bstcon::response> response = client_->operator() (oauth::utility::get_host(uri),buf,ec);
     
-    const int status_code = client_->get_response()->status_code;
+    const int status_code = response->status_code;
     if(200 <= status_code && status_code < 300)
     {
-      const Param_Type response = oauth::utility::parse_urlencoded(client_->get_response()->body);
-      key_->set_access_token (oauth::utility::url_decode(response.at("oauth_token")));
-      key_->set_access_secret(oauth::utility::url_decode(response.at("oauth_token_secret")));
+      const Param_Type parsed = oauth::utility::parse_urlencoded(response->body);
+      key_->set_access_token (oauth::utility::url_decode(parsed.at("oauth_token")));
+      key_->set_access_secret(oauth::utility::url_decode(parsed.at("oauth_token_secret")));
     }
 
     return;
@@ -119,9 +121,9 @@ public:
       ("oauth_signature",oauth::utility::get_signature(method,parsed_uri.get<0>()+"://"+parsed_uri.get<1>()+parsed_uri.get<2>(),key_->get_signature_key(),signature_param));
 
     const std::string body_string = (method!="GET") ? oauth::utility::get_urlencoded(params) : "";
-    boost::asio::streambuf buf;
+    boost::shared_ptr<boost::asio::streambuf> buf(new boost::asio::streambuf());
     {
-      std::ostream os(&buf);
+      std::ostream os(buf.get());
       os << method << " " << parsed_uri.get<2>() << ((method=="GET") ? "?"+oauth::utility::get_urlencoded(params) : "") << " HTTP/1.1" << "\r\n";
       os << "Host: " << parsed_uri.get<1>() << "\r\n";
       os << "Content-Length: " << body_string.size() << "\r\n";
@@ -131,20 +133,23 @@ public:
     }
 
     boost::system::error_code ec;
-    client_->operator() (parsed_uri.get<1>(),buf,ec);
+    const boost::shared_ptr<bstcon::response> response =  client_->operator() (parsed_uri.get<1>(),buf,ec);
 
-    //const int status_code = client_->get_response()->status_code_;
-    /*if(200 <= status_code && status_code < 300)
-    {
-      const Param_Type response = oauth::utility::parse_urlencoded(client_->get_response()->body_);
-      key_->set_access_token (response.at("oauth_token"));
-      key_->set_access_secret(response.at("oauth_token_secret"));
-    }*/
-
-    return client_->get_response();
+    return response;
   }
 
 protected:
+  void set_access_token(const boost::shared_ptr<bstcon::response> response,const boost::system::error_code& ec)
+  {
+    if(ec) return;
+    if(200 <= response->status_code && response->status_code < 300)
+    {
+      const Param_Type parsed = oauth::utility::parse_urlencoded(response->body);
+      key_->set_access_token (oauth::utility::url_decode(parsed.at("oauth_token")));
+      key_->set_access_secret(oauth::utility::url_decode(parsed.at("oauth_token_secret")));
+    }
+    return;
+  }
 
   boost::shared_ptr<Key_Type> key_;
   boost::shared_ptr<bstcon::client> client_;
--- sample/sample.cpp
+++ sample/sample.cpp
@@ -8,129 +8,155 @@
 /*#include <boost/thread.hpp>
 #include <boost/property_tree/ptree.hpp>
 */
+#include <babel.h>
 
 int main()
 {
+  babel::init_babel();
+
   //ただの準備です
   boost::asio::io_service io_service;
   boost::asio::ssl::context ctx(io_service,boost::asio::ssl::context_base::sslv3_client); //SSL用
   
-  //yahoo
-  /*{
+//  //yahoo
+//  /*{
+//    boost::shared_ptr<oauth::keys::key_version1> key(
+//      new oauth::keys::key_version1("consumer_key","consumer_secret"));
+//    oauth::client<oauth::version::v1> client(
+//      key,
+//      boost::shared_ptr<bstcon::client>(new bstcon::client(io_service))
+//      );
+//  
+//
+//    client.get_request_token("POST","http://auth.login.yahoo.co.jp/oauth/v2/get_request_token");
+//    const std::string auth_uri = "http://auth.login.yahoo.co.jp/oauth/v2/request_auth?oauth_token=" + key->get_access_token();
+//
+//    std::string pin;
+//    std::cin >> pin;
+//
+//    client.get_access_token("POST","http://auth.login.yahoo.co.jp/oauth/v2/get_token",pin);
+//  }
+//  */
+
+    {
+    boost::asio::io_service io_service;
+    boost::asio::ssl::context ctx(io_service,boost::asio::ssl::context_base::sslv3_client); //SSL用
+
     boost::shared_ptr<oauth::keys::key_version1> key(
-      new oauth::keys::key_version1("consumer_key","consumer_secret"));
-    oauth::client<oauth::version::v1> client(
-      key,
-      boost::shared_ptr<bstcon::client>(new bstcon::client(io_service))
-      );
-  
-
-    client.get_request_token("POST","http://auth.login.yahoo.co.jp/oauth/v2/get_request_token");
-    const std::string auth_uri = "http://auth.login.yahoo.co.jp/oauth/v2/request_auth?oauth_token=" + key->get_access_token();
-
-    std::string pin;
-    std::cin >> pin;
-
-    client.get_access_token("POST","http://auth.login.yahoo.co.jp/oauth/v2/get_token",pin);
-  }
-  */
-
-  {
-    boost::shared_ptr<oauth::keys::key_version1> key(
-      new oauth::keys::key_version1("consumer_key","consumer_secret"));
+          new oauth::keys::key_version1("consumer_key","consumer_secret"));
     oauth::twitter client(
       key,
-      boost::shared_ptr<bstcon::client>(new bstcon::client(io_service,ctx))
+      boost::shared_ptr<bstcon::client>(new bstcon::client(io_service,ctx,bstcon::connection_type::async))
       );
 
     // XAuth
     client.get_xauth_token("user_id","user_password");
+    io_service.run();
+    io_service.reset();
 
     std::map<std::string,std::string> params = boost::assign::map_list_of
-      ("status","hoge");
+      ("status",babel::sjis_to_utf8("テスト"));
     const boost::shared_ptr<bstcon::response> response
-      = client.request_urlencoded("POST","http://api.twitter.com/1/statuses/update.xml",params);
+      = client.request_urlencoded("POST","https://api.twitter.com/1/statuses/update.xml",params);
+    io_service.run();
+
   }
-
-  //twitter
-  /*
-  {
-    boost::shared_ptr<oauth::keys::key_version1> key(
-      new oauth::keys::key_version1("consumer_key","consumer_secret"));
-    oauth::client<oauth::version::v1> client(
-      key,
-      boost::shared_ptr<bstcon::client>(new bstcon::client(io_service))
-      );
-  
-
-    client.get_request_token("POST","http://api.twitter.com/oauth/request_token");
-    const std::string auth_uri = "http://api.twitter.com/oauth/authorize?oauth_token=" + key->get_access_token();
-
-    std::string pin;
-    std::cin >> pin;
-
-    client.get_access_token("POST","http://api.twitter.com/oauth/access_token",pin);
-
-    std::map<std::string,std::string> params = boost::assign::map_list_of
-      ("status","hoge");
-    const boost::shared_ptr<bstcon::response> response
-      = client.request_urlencoded("POST","http://api.twitter.com/1/statuses/update.xml",params);
-  }*/
-
-  //boost::system::error_code ec;
-  //const std::string host("www.hatena.ne.jp");
-  //boost::asio::streambuf buf;
-  //std::ostream os(&buf);
-  //os << "GET / HTTP/1.1\r\nHost: " << host << "\r\nConnection: close\r\n\r\n"; //ただのリクエスト
-
-  ////
-  //// 同期・HTTP通信
-  ////
-  //{
-  //  bstcon::client client(io_service/*,oauth::protocol::connection_type::sync*/);
-  //  client(host,buf,ec);
-  //  
-  //  //終了
-  //}
-
-  ////
-  //// 同期・SSL通信
-  ////
-  //{
-  //  bstcon::client client(io_service,ctx/*,oauth::protocol::connection_type::sync*/);
-  //  client(host,buf,ec);
-
-  //  //終了
-  //}
-  
-  ////
-  //// 非同期・HTTP通信
-  ////
-  //{
-  //  bstcon::client client(io_service,oauth::protocol::connection_type::async);
-  //  client(host,buf,ec);
-
-  //  //何か
-
-  //  io_service.run(); //通信を走らせる
-
-  //  //終了
-  //  return 0;
-  //}
-
-  ////
-  //// 非同期・SSL通信
-  ////
-  //{
-  //  bstcon::client client(io_service,ctx,oauth::protocol::connection_type::async);
-  //  client(host,buf,ec);
-
-  //  //何か
-
-  //  io_service.run(); //通信を走らせる
-
-  //  //終了
-  //}
+//  //{
+//  //  boost::shared_ptr<oauth::keys::key_version1> key(
+//  //    new oauth::keys::key_version1("consumer_key","consumer_secret"));
+//  //  oauth::twitter client(
+//  //    key,
+//  //    boost::shared_ptr<bstcon::client>(new bstcon::client(io_service,ctx))
+//  //    );
+//
+//  //  // XAuth
+//  //  client.get_xauth_token("user_id","user_password");
+//
+//  //  std::map<std::string,std::string> params = boost::assign::map_list_of
+//  //    ("status","hoge");
+//  //  const boost::shared_ptr<bstcon::response> response
+//  //    = client.request_urlencoded("POST","http://api.twitter.com/1/statuses/update.xml",params);
+//  //}
+//
+//  //twitter
+//  /*
+//  {
+//    boost::shared_ptr<oauth::keys::key_version1> key(
+//      new oauth::keys::key_version1("consumer_key","consumer_secret"));
+//    oauth::client<oauth::version::v1> client(
+//      key,
+//      boost::shared_ptr<bstcon::client>(new bstcon::client(io_service))
+//      );
+//  
+//
+//    client.get_request_token("POST","http://api.twitter.com/oauth/request_token");
+//    const std::string auth_uri = "http://api.twitter.com/oauth/authorize?oauth_token=" + key->get_access_token();
+//
+//    std::string pin;
+//    std::cin >> pin;
+//
+//    client.get_access_token("POST","http://api.twitter.com/oauth/access_token",pin);
+//
+//    std::map<std::string,std::string> params = boost::assign::map_list_of
+//      ("status","hoge");
+//    const boost::shared_ptr<bstcon::response> response
+//      = client.request_urlencoded("POST","http://api.twitter.com/1/statuses/update.xml",params);
+//  }*/
+//
+//  //boost::system::error_code ec;
+//  //const std::string host("www.hatena.ne.jp");
+//  //boost::asio::streambuf buf;
+//  //std::ostream os(&buf);
+//  //os << "GET / HTTP/1.1\r\nHost: " << host << "\r\nConnection: close\r\n\r\n"; //ただのリクエスト
+//
+//  ////
+//  //// 同期・HTTP通信
+//  ////
+//  //{
+//  //  bstcon::client client(io_service/*,oauth::protocol::connection_type::sync*/);
+//  //  client(host,buf,ec);
+//  //  
+//  //  //終了
+//  //}
+//
+//  ////
+//  //// 同期・SSL通信
+//  ////
+//  //{
+//  //  bstcon::client client(io_service,ctx/*,oauth::protocol::connection_type::sync*/);
+//  //  client(host,buf,ec);
+//
+//  //  //終了
+//  //}
+//  
+//  ////
+//  //// 非同期・HTTP通信
+//  ////
+//  //{
+//  //  bstcon::client client(io_service,oauth::protocol::connection_type::async);
+//  //  client(host,buf,ec);
+//
+//  //  //何か
+//
+//  //  io_service.run(); //通信を走らせる
+//
+//  //  //終了
+//  //  return 0;
+//  //}
+//
+//  ////
+//  //// 非同期・SSL通信
+//  ////
+//  //{
+//  //  bstcon::client client(io_service,ctx,oauth::protocol::connection_type::async);
+//  //  client(host,buf,ec);
+//
+//  //  //何か
+//
+//  //  io_service.run(); //通信を走らせる
+//
+//  //  //終了
+//  //}
   
   return 0;
 }
