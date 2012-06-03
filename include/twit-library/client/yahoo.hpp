ea923feb8bdfdae6e203f9ac37528568e9f8899b
godai_0519 <godai0519@gmail.com>
2012-06-03 11:40:05 +0900

    Some Modified
    Signed-off-by: godai_0519 <godai0519@gmail.com>

---
+++ include/twit-library/client/yahoo.hpp
@@ -0,0 +1,43 @@
+//
+// yahoo.hpp
+// ~~~~~~~~~~
+//
+// Yahoo URL List‚Æ‚»‚ÌŽÀ—p‰»
+//
+
+#ifndef TWIT_LIB_YAHOO
+#define TWIT_LIB_YAHOO
+
+#include "common_v1.hpp"
+
+namespace oauth{
+
+struct yahoo_set{
+  static std::string get_host()
+  {
+    return "auth.login.yahoo.co.jp";
+  }
+  static std::string get_request_method()
+  {
+    return "POST";
+  }
+  static std::string get_request_path()
+  {
+    return "/oauth/v2/get_request_token";
+  }
+  static std::string get_access_method()
+  {
+    return "POST";
+  }
+  static std::string get_access_path()
+  {
+    return "/oauth/v2/get_token";
+  }
+};
+
+typedef oauth::common_v1<oauth::yahoo_set> yahoo;
+
+//} // namespace version
+} // namespace oauth
+
+#endif
