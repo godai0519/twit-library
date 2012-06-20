78e467bd5be1bc0c8e0339bb68ee550d9606f812
godai_0519 <godai0519@gmail.com>
2012-06-20 22:56:25 +0900

    Developping. Unstable Version.
    Signed-off-by: godai_0519 <godai0519@gmail.com>

--- include/twit-library/keys/key_version2.hpp
+++ include/twit-library/keys/key_version2.hpp
@@ -8,28 +8,34 @@
 #ifndef TWIT_LIB_KEYS_VERSION_2
 #define TWIT_LIB_KEYS_VERSION_2
 
-#include "key_base.hpp"
+#include <string>
+#include <boost/noncopyable.hpp>
 
 namespace oauth{
 namespace keys{
 
-class key_version2 : public key_base{
+class key_version2 : boost::noncopyable{
+  //std::string response_type_ = "code"; //(REQUIRED)
+  std::string client_id_; //(REQUIRED)
+  std::string client_secret_; //(REQUIRED)
+  std::string access_token_;
+  //std::string redirect_uri_ = "urn:ietf:wg:oauth:2.0:oob"; //(REQUIRED)
+
 public:
   key_version2(){}
-  virtual ~key_version2(){}
+  key_version2(const std::string& client_id,const std::string& client_secret)
+    : client_id_(client_id), client_secret_(client_secret){}
+  ~key_version2(){}
 
   //setter
   void set_client_id(const std::string& client_id){ client_id_ = client_id; }
+  void set_client_secret(const std::string& client_secret){ client_secret_ = client_secret; }
+  void set_access_token   (const std::string& access_token)   { access_token_ = access_token; }
 
   //getter
-  std::string get_client_id(){ return client_id_; }
-  std::string get_signature_key(){ return client_id_;}; //tekito-
-
-
-private:
-  //std::string response_type_ = "code"; //(REQUIRED)
-  std::string client_id_; //(REQUIRED)
-  //std::string redirect_uri_ = "urn:ietf:wg:oauth:2.0:oob"; //(REQUIRED)
+  const std::string get_client_id() const { return client_id_; }
+  const std::string get_client_secret() const { return client_secret_; }
+  const std::string get_access_token() const { return access_token_; }
 };
 
 } // namespace keys
